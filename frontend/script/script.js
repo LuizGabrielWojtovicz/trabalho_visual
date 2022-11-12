f4681ddb439a703773a1fae2279eb26560ee2e99
Lugaw <83958914+LuizGabrielWojtovicz@users.noreply.github.com>
2022-11-12 12:31:46 -0300

    Add files via upload

---
+++ frontend/script/script.js
@@ -0,0 +1,70 @@
+
+/**
+ * Funções
+ */
+
+//atendimento
+
+const cadastrar_atendimento = () =>
+{
+	//codigo
+}
+
+const mostrar_atendimento = () =>
+{
+	//codigo
+}
+
+const listar_atentimentos = () =>
+{
+	//codigo
+}
+
+const excluir_atendimento = () =>
+{
+	//codigo
+}
+
+//funcionario
+
+const cadastrar_funcionario = () =>
+{
+	//codigo
+}
+
+const mostrar_funcionario = () =>
+{
+	//codigo
+}
+
+const listar_funcionarios = () =>
+{
+	//codigo
+}
+
+const excluir_funcionario = () =>
+{
+	//codigo
+}
+
+//cliente
+
+const cadastrar_cliente = () =>
+{
+	//codigo
+}
+
+const mostrar_cliente = () =>
+{
+	//codigo
+}
+
+const listar_clientes = () =>
+{
+	//codigo
+}
+
+const excluir_cliente = () =>
+{
+	//codigo
+}
