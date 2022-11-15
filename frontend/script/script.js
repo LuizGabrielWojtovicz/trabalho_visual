273420a9d8a92de043d1b13d069feab1eb4aafb9
Lugaw <83958914+LuizGabrielWojtovicz@users.noreply.github.com>
2022-11-15 11:53:20 -0300

    Update script.js

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -7,7 +7,16 @@
 
 const cadastrar_atendimento = () =>
 {
-	//codigo
+	let idCliente = document.getElementById('id-cliente')
+	let idFuncionario = document.getElementById('id-funcionario')
+	let tipo = document.getElementById('tipo-atendimento')
+	let data = document.getElementById('data-atendimento')
+
+	if (!valida_id(idCliente) || !valida_id(idFuncionario) || !valida_cargo_ou_tipo(tipo) || !valida_data(data)) {
+		return
+	}
+
+	console.log(idCliente.value, idFuncionario.value, tipo.value, data.value)
 }
 
 const mostrar_atendimento = () =>
@@ -77,7 +86,16 @@ const excluir_atendimento = () =>
 
 const cadastrar_funcionario = () =>
 {
-	//codigo
+	let nome = document.getElementById('nome-funcionario')
+	let telefone = document.getElementById('telefone-funcionario')
+	let email = document.getElementById('email-funcionario')
+	let cargo = document.getElementById('cargo-funcionario')
+
+	if (!valida_nome(nome) || !valida_telefone(telefone) || !valida_email(email) || !valida_cargo_ou_tipo(cargo)) {
+		return
+	}
+
+	console.log(nome.value, telefone.value, email.value, cargo.value)
 }
 
 const mostrar_funcionario = () =>
@@ -147,7 +165,15 @@ const excluir_funcionario = () =>
 
 const cadastrar_cliente = () =>
 {
-	//codigo
+	let nome = document.getElementById('nome-cliente')
+	let telefone = document.getElementById('telefone-cliente')
+	let email = document.getElementById('email-cliente')
+
+	if (!valida_nome(nome) || !valida_telefone(telefone) || !valida_email(email)) {
+		return
+	}
+
+	console.log(nome.value, telefone.value, email.value)
 }
 
 const mostrar_cliente = () =>
